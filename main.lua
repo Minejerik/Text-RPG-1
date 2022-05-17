1ab68909a36a63c72301a343dafe080a4e5d9d8e
Minejerik <71468014+Minejerikisme@users.noreply.github.com>
2022-05-17 21:00:42 +0000

    xxx

--- main.lua
+++ main.lua
@@ -32,11 +32,10 @@ print("Hello",(Name),"!")
 
 
 local function main_loop()
-io.write("\nWhat would you like to do?\n\n")
+io.write("\nWhat would you like to do?\n")
 input = io.read()
 
 if input == "work" then
-os.execute("clear")
   if Energy > 0 then
 	Money=Money+5*Promotion
   Energy=Energy-1
@@ -48,13 +47,9 @@ os.execute("clear")
 end
 
 if input == "eat" then
-os.execute("clear")
 Ammount = 1
 io.write("Ammount?\n")
 Ammount = io.read()
-if Ammount == "all" then
-Ammount = Food
-end
   if Ammount*Food>=0 then
   Health=Health+5*Ammount
   Food=Food-1*Ammount
@@ -69,7 +64,7 @@ Ammount = 1
 end
 
 if input=="fight" then
-os.execute("clear")
+
     loss=math.random(0,40)
     gain=math.random(5,Strength/2)
 	Health=Health-loss
@@ -81,12 +76,11 @@ os.execute("clear")
 end
 
 if input=="info"then
-		os.execute("clear")
 cmd.info()
 end
 
 if input== "train" then
-		os.execute("clear")
+
   Strength=Strength+5
   Energy=Energy-5
   print("Strength =",(Strength))
@@ -94,7 +88,6 @@ if input== "train" then
 end
 
 if input == "ask_for_promotion" then
-		os.execute("clear")
 if Energy >= 10 then
 Energy = Energy-10
 Chance = math.random(0,1)
@@ -111,36 +104,31 @@ end
 end
 
 if input=="buy_food" then
-		os.execute("clear")
 buy.food()
 end
 
 --car stuff
 if input=="buy_car" then
-		os.execute("clear")
 buy.car()
 end
 
 --motorcycle stuff
 if input=="buy_motorcycle" then
-		os.execute("clear")
 buy.motorcycle()
 end
 
 --clothes stuff
 if input=="buy_clothes" then
-		os.execute("clear")
+
 buy.clothes()
 end
 
 --house stuff
 if input=="buy_house" then
-		os.execute("clear")
 buy.house()
 end
 
 if input == "theodore_roosevelt" then
-		os.execute("clear")
 	Money=9999
   Energy=9999
   Food=9999
@@ -149,19 +137,16 @@ end
 
 
 if input=="help" then
-		os.execute("clear")
 cmd.help()
 end
 
 if Drip >= 1000 then
-		os.execute("clear")
 	print("You Win! You Became Drippy!")
   os.exit()
 end
 
 	
 if Money<10 and Food==0 and Energy==0 then
-		os.execute("clear")
 print("You Got Soft Locked! You Have Been Saved!")
 Soft_Locked = 1
 Money = Money+20
@@ -174,11 +159,9 @@ end
 	
 
 if Health <= 0 then
-		os.execute("clear")
 	print("You Died! You Lost All Your Health!")
   os.exit()
 end
 main_loop()
 end
 main_loop()
-os.execute("clear")
